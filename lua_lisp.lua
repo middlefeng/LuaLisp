c912ccbe8bc2862b34f92ce3ee3ff8310c2e49de
Dong Feng <middle.fengdong@gmail.com>
2015-01-24 21:12:58 -0800

    Add command line Lisp.

---
+++ lua_lisp.lua
@@ -0,0 +1,22 @@
+
+
+local quote = require "quote"
+local eval = require "eval"
+local lisp = require "lisp"
+
+local filename = ...
+local file = io.open(filename, "r")
+
+if not(file) or io.type(file) ~= "file" then
+	print("File not found: " .. filename)
+	os.exit()
+end
+
+local lisp_src = file:read("a")
+local s_exp = quote.quote(lisp_src)
+
+local env = eval.Enviornment.initEnviornment()
+local value = env:eval(lisp.car(s_exp))
+
+print("> " .. tostring(value))
+
