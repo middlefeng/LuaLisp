9feca28e3201a918cd2d111b1490ec4ff8611d08
Dong Feng <middle.fengdong@gmail.com>
2015-02-02 23:59:50 -0800

    Add test to lambda.

---
+++ Test/test4.lisp
@@ -0,0 +1,19 @@
+
+
+(define v1 '(a))
+
+(define (append_v1 e)
+		(set! v1 (cond ((null? e) v1)
+			   		   ((atom? e) (cons e v1))
+			  		   (else (cons (car e) (append_v1 (cdr e))))))
+		v1)
+
+;(append_v1 '(b c))
+
+(define (append_list exp)
+		(lambda (exp_outter)
+		 		(append_v1 exp_outter)
+		 		(append_v1 exp)))
+
+;((lambda () (append_v1  '(1 2))))
+((append_list '(11 22)) '(33 44))
