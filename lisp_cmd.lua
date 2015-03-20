b3ded4e86f38cfb86370d3061fdb0708e3f3e9c6
Dong Feng <middle.fengdong@gmail.com>
2015-03-20 16:25:34 -0700

    Interactive REPL.

---
+++ lisp_cmd.lua
@@ -0,0 +1,25 @@
+
+
+local quote = require "quote"
+local eval = require "eval_cont"
+local lisp = require "lisp"
+
+
+local cmd
+local env = eval.Environment.initEnv()
+
+local function func_noop() end
+
+repeat
+	io.write("> ")
+	cmd = io.read("l")
+
+	if cmd ~= "exit" then
+		local bottom_cont = eval.ContinuationBottom:new(func_noop)
+		local s_exp = quote.quote(cmd)
+		local value = eval.eval_begin(s_exp, env, bottom_cont)
+		print(value)
+	end
+
+until cmd == "exit"
+
