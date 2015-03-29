e2c66a0973a29e18c5864199b37a60a3bdcbc0be
Dong Feng <middle.fengdong@gmail.com>
2015-03-29 14:56:31 -0700

    Benchmark support library. Clean up the organization on C libraries.

---
+++ lib/luaReadline/luaReadline.c
@@ -0,0 +1,51 @@
+//
+//  luaReadline.c
+//  luaReadline
+//
+//  Created by Middleware on 3/20/15.
+//  Copyright (c) 2015 fengdong. All rights reserved.
+//
+
+#include "luaReadline.h"
+
+#include "lua.h"
+#include "lauxlib.h"
+
+#include <string.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+static int lua_readline(lua_State* L);
+
+
+static const luaL_Reg extension[] =
+{
+	"readline", lua_readline,
+	NULL, NULL,
+};
+
+
+
+static int lua_readline(lua_State* L)
+{
+	const char* prompt = lua_tostring(L, -1);
+	
+	char* cmd;
+	cmd = readline(prompt);
+	
+	if (cmd && strlen(cmd) > 0)
+		add_history(cmd);
+	
+	lua_pushstring(L, cmd);
+	return 1;
+}
+
+
+
+int luaopen_luaReadline(lua_State *L)
+{
+	luaL_newlib(L, extension);
+	return 1;
+}
