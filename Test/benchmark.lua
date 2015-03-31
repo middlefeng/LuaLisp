43e0296c5ee787cc6ac0c7f43f9b78b06b2d5509
Dong Feng <middle.fengdong@gmail.com>
2015-03-31 13:13:20 -0700

    Benchmark the cursive version in Lua.

--- Test/benchmark.lua
+++ Test/benchmark.lua
@@ -1,34 +1,18 @@
 
 
---[[
-int factorCount (long n)
-{
-    double square = sqrt (n);
-    int isquare = (int) square;
-    int count = isquare == square ? -1 : 0;
-    long candidate;
-    for (candidate = 1; candidate <= isquare; candidate ++)
-        if (0 == n % candidate) count += 2;
-    return count;
-}
 
-int main ()
-{
-    long triangle = 1;
-    int index = 1;
-    while (factorCount (triangle) < 1001)
-    {
-        index ++;
-        triangle += index;
-    }
-    printf ("%ld\n", triangle);
-}
-]]--
 
 
 package.cpath = package.cpath .. ";./lib/target/lib?.dylib"
 local benchmark = require "benchmark"
 
+
+
+------------------------------------------------------------------------
+--
+--   Regular Lua Version
+--
+------------------------------------------------------------------------
 
 
 function factorCount(n)
@@ -64,6 +48,73 @@ print("Triangle: " .. triangle .. " takes " .. time)
 
 
 
+------------------------------------------------------------------------
+--
+--   Recursive Lua Version
+--
+------------------------------------------------------------------------
+
+
+
+local function factorCount_recur(n)
+    local square = math.sqrt(n)
+    local isquare = math.floor(square)
+    local count = (isquare == square) and -1 or 0
+
+    local function doCount(candidate, count)
+        if candidate == isquare then
+            if math.fmod(n, candidate) == 0 then
+                return count + 2
+            else
+                return count
+            end
+        else
+            if math.fmod(n, candidate) == 0 then
+                return doCount(candidate + 1, count + 2)
+            else
+                return doCount(candidate + 1, count)
+            end
+        end
+    end
+
+    return doCount(1, count)
+end
+
+
+
+benchmark.begin_run();
+
+
+local function doMain(index, triangle)
+    if factorCount_recur(triangle) < iterate_num then
+        return doMain(index + 1, index + 1 + triangle)
+    else
+        return triangle
+    end
+end
+
+triangle = doMain(1, 1)
+
+
+time = benchmark.end_run()
+
+print("(Recursive Version) Triangle: " .. triangle .. " takes " .. time)
+
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------
+--
+--   Lisp Version
+--
+------------------------------------------------------------------------
+
+
 
 local quote = require "quote"
 local eval = require "eval_cont"
@@ -82,8 +133,8 @@ end
 local lisp_src = file:read("a")
 local s_exp = quote.quote(lisp_src)
 
-print("Source: ")
-print(lisp.list_tostring(s_exp, true) .. "\n")
+-- print("Source: ")
+-- print(lisp.list_tostring(s_exp, true) .. "\n")
 
 local env = eval.Environment.initEnv()
 local bottom_cont = eval.ContinuationBottom:new(print)
